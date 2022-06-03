2d59afa43458d31a8cd5a478d49453cd0d6ac7c4
Cyanne Jones <98280256+Cyanne-Jones@users.noreply.github.com>
2022-06-03 16:33:56 -0600

    Add files via upload
    Initial commit

---
+++ star-wars-iterator-practice.js
@@ -0,0 +1,27 @@
+//Welcome to Star Wars iterator practice!! .
+//Just a quick note: I tried as hard as I could to keep this as accurate as possible. Please don't fight me if I got something wrong. 🙏🏼
+//ALSO!! 
+//Everyone's birthdays are measrured in BBY! (Before the Battle of Yavin) 
+// Because of this, the dates go backwards (like BC in our timeline), so the bigger the number, the earlier the critter was born!
+// May the force be with you
+
+const galaxy = {
+  coreWorlds: ['Coruscant', 'Talravin', 'Alderaan'],
+  innerRim: ['Manaan', 'Jakku'],
+  unknownRegions: ['Exegol', 'Stewjon', 'Rakata Prime'],
+  outerRim: ['Tatooine', 'Kamino', 'Korriban', 'Telos', 'Taris'],
+  midRim: ['Naboo', 'Kashyyk']
+};
+
+const critters = [
+  {
+    name: 'Anakin Skywalker',
+    birthYear: 41,
+    homePlanet: 'tatooine'
+  },
+  {
+    name: 'Obi Wan Kenobi',
+    birthYear: 57,
+    homePlanet: 'stewjon'
+  },
+]
